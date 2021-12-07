b8f38ad78b455c87a071ff3d257fad363e623513
pany <939630029@qq.com>
2021-12-07 11:58:39 +0800

    chore: 初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# v3-flutter-template
-flutter template
+Flutter 基础脚手架
+
+## 依赖包
+
+`flutter packages get` 或 `flutter pub get`
+
+## 分析代码
+
+`flutter analyze`
+
+## 运行项目
+
+`flutter run`
+
+## 如果遇到着色器渲染错误（Shader compilation error），可以运行 clean 后再 run
+
+`flutter clean`
+
+## 安卓真机调试
+`flutter devices`
+`flutter run`
+
+## 安卓打包
+`flutter build apk`
+
+## 开发环境
+
+1. Flutter version 2.5.3
+2. Dart version 2.14.4
+3. Android SDK version 31.0.0
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_templete/pages/login/index.dart';
+import 'package:flutter_templete/utils/sp.dart';
+
+void main() {
+  // 网格线
+  // debugPaintSizeEnabled = true;
+  // Flutter 版本 (1.12.13+hotfix.5) 后，初始化插件必须加 ensureInitialized
+  // WidgetsFlutterBinding.ensureInitialized();
+  // 应用入口
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter_templete',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo, // 主题
+      ),
+      initialRoute: "home",
+      onGenerateRoute: onGenerateRoute,
+      localizationsDelegates: [
+        // 本地化的代理类
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [Locale('zh', 'CH')],
+    );
+  }
+
+  // 路由钩子
+  Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    String? routeName;
+    routeName = routeBeforeHook(settings);
+    return MaterialPageRoute(builder: (context) {
+      /// 注意：如果路由的形式为: '/a/b/c'
+      /// 那么将依次检索 '/' -> '/a' -> '/a/b' -> '/a/b/c'
+      /// 所以，这里的路由命名最好避免使用 '/xxx' 形式
+      switch (routeName) {
+        case "login":
+          return LoginPage();
+        default:
+          return Scaffold(
+            body: Center(
+              child: Text("页面不存在"),
+            ),
+          );
+      }
+    });
+  }
+
+  // 路由拦截器
+  String? routeBeforeHook(RouteSettings settings) {
+    final token = SP.getToken() ?? '';
+    if (token != '') {
+      if (settings.name == 'login') {
+        return 'home';
+      }
+      return settings.name;
+    }
+    return 'login';
+  }
+}
