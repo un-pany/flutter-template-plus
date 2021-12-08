d548621dc4fae34f605b983418819732e0ac8409
pany <939630029@qq.com>
2021-12-08 12:43:45 +0800

    perf: 优化 http 层

--- lib/http/request/base_request.dart
+++ lib/http/request/base_request.dart
@@ -4,17 +4,21 @@ enum HttpMethod { GET, POST, DELETE, PUT }
 abstract class BaseRequest {
   // path 参数
   var pathParams;
-  // 是否为 https
+  // 是否为 https，默认为 true
   bool useHttps = true;
-  // 域名
-  String authority() {
-    return 'api.devio.org';
-  }
 
   // 设置请求方法
   HttpMethod httpMethod();
   // 设置 path
   String path();
+  // 设置接口是否需要登录
+  bool needLogin();
+
+  // 默认域名
+  String authority() {
+    return 'api.devio.org';
+  }
+
   // 生成具体的 url
   String url() {
     Uri uri;
@@ -37,10 +41,7 @@ abstract class BaseRequest {
     return uri.toString();
   }
 
-  // 接口是否需要登录
-  bool needLogin();
-
-  // 查询参数
+  // 默认查询参数
   Map<String, String> params = Map();
   // 添加查询参数
   BaseRequest add(String k, Object v) {
@@ -48,9 +49,9 @@ abstract class BaseRequest {
     return this;
   }
 
-  // header 数据
+  // 默认 header 数据
   Map<String, String> header = Map();
-  // 添加 header
+  // 添加 header 数据
   BaseRequest addHeader(String k, Object v) {
     params[k] = v.toString();
     return this;
