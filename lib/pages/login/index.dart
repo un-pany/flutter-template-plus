73a3304f693a84e8e5151fffa71487c4d09062df
pany <939630029@qq.com>
2021-12-11 21:50:28 +0800

    refactor: login_input & login page

--- lib/pages/login/index.dart
+++ lib/pages/login/index.dart
@@ -11,38 +11,130 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final GlobalKey _formKey = GlobalKey<FormState>();
+  TextEditingController _userController = TextEditingController(text: '');
+  TextEditingController _passwordController = TextEditingController(text: '');
+  // '记住密码' 复选框
+  bool? _savePassword = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.only(
+          left: 35,
+          right: 35,
+        ),
         // ListView 可以自适应键盘，防止键盘弹起后遮挡
         child: ListView(
           children: [
-            LoginInput(
+            Image.asset(
+              'static/images/login/logo.png',
+              width: 200,
+              height: 200,
+            ),
+            _form(),
+            _save(),
+            _button(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // 表单
+  Widget _form() {
+    return Form(
+      // 设置 globalKey，用于后面获取 FormState
+      key: _formKey,
+      // 不开启自动校验表单，而是选择在点击登录按钮时校验
+      autovalidateMode: AutovalidateMode.disabled,
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+              bottom: 20,
+            ),
+            child: LoginInput(
               '请输入用户名',
               Icon(Icons.perm_identity),
-            ),
-            LoginInput(
-              '请输入密码',
-              Icon(Icons.lock_outline),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              child: Text("发送请求"),
-              onPressed: () async {
-                try {
-                  var res = await LoginDao.login('admin', '123456');
-                  print('page层收到的数据:$res');
-                } on NeedLogin catch (e) {
-                  print(e);
-                } on NeedAuth catch (e) {
-                  print(e);
-                } on MyNetError catch (e) {
-                  print(e);
-                }
+              _userController,
+              validator: (value) {
+                return value!.trim().length > 0 ? null : "用户名不能为空";
               },
             ),
-          ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              bottom: 20,
+            ),
+            child: LoginInput(
+              '请输入密码',
+              Icon(Icons.lock_outline),
+              _passwordController,
+              obscureText: true,
+              validator: (value) {
+                return value!.trim().length >= 8 ? null : "密码不能少于8位";
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // 记住密码
+  _save() {
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: 50,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Checkbox(
+            value: _savePassword,
+            onChanged: (value) {
+              setState(() {
+                _savePassword = value;
+              });
+            },
+          ),
+          Text("记住密码"),
+        ],
+      ),
+    );
+  }
+
+  // 登录按钮
+  Widget _button() {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 80,
+        right: 80,
+      ),
+      child: SizedBox(
+        height: 45,
+        child: ElevatedButton(
+          child: Text("登录"),
+          onPressed: () async {
+            if ((_formKey.currentState as FormState).validate()) {
+              // 验证通过提交数据
+              try {
+                var res = await LoginDao.login(
+                  _userController.text.trim(),
+                  _passwordController.text.trim(),
+                );
+                print('page层收到的数据:$res');
+              } on NeedLogin catch (e) {
+                print(e);
+              } on NeedAuth catch (e) {
+                print(e);
+              } on MyNetError catch (e) {
+                print(e);
+              }
+            }
+          },
         ),
       ),
     );
--- lib/widgets/login_input.dart
+++ lib/widgets/login_input.dart
@@ -1,67 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 /// 登录输入框
-class LoginInput extends StatefulWidget {
+class LoginInput extends StatelessWidget {
   // 提示文字
   final String hintText;
   // 前缀图标
   final Widget? prefixIcon;
+  // 控制器
+  final TextEditingController controller;
   // 最多输入数，有值后右下角就会有一个计数器
   final int maxLength;
   // 隐藏文本
   final bool obscureText;
   // 键盘类型
   final TextInputType keyboardType;
+  // 校验规则
+  final FormFieldValidator<String>? validator;
 
   const LoginInput(
     this.hintText,
-    this.prefixIcon, {
+    this.prefixIcon,
+    this.controller, {
     Key? key,
     this.maxLength = 18,
     this.obscureText = false,
     this.keyboardType = TextInputType.text,
+    this.validator,
   }) : super(key: key);
 
   @override
-  _LoginInputState createState() => _LoginInputState();
-}
-
-class _LoginInputState extends State<LoginInput> {
-  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        left: 30,
-        right: 30,
-      ),
-      child: TextField(
-        maxLength: widget.maxLength,
-        keyboardType: widget.keyboardType,
-        obscureText: widget.obscureText,
-        // decoration 设置输入框的样式
-        decoration: InputDecoration(
-          counterText: '', // 隐藏计数器
-          hintText: widget.hintText,
-          prefixIcon: widget.prefixIcon,
-          // 背景颜色，结合 filled: true，才有效
-          fillColor: Colors.grey[200],
-          filled: true,
-          border: OutlineInputBorder(
-            // 圆角形
-            borderRadius: BorderRadius.all(
-              Radius.circular(32),
-            ),
-            // 去除边框
-            borderSide: BorderSide.none,
+    // Form 的子元素必须是 TextFormField，TextFormField 具备 TextField 的所有属性
+    return TextFormField(
+      controller: controller,
+      maxLength: maxLength,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      // decoration 设置输入框的样式
+      decoration: InputDecoration(
+        counterText: '', // 隐藏计数器
+        hintText: hintText,
+        prefixIcon: prefixIcon,
+        // 背景颜色，结合 filled: true，才有效
+        fillColor: Colors.grey[200],
+        filled: true,
+        border: OutlineInputBorder(
+          // 圆角形
+          borderRadius: BorderRadius.all(
+            Radius.circular(8),
           ),
-          // 内容内边距
-          contentPadding: EdgeInsets.only(
-            top: 0,
-            bottom: 0,
-          ),
+          // 去除边框
+          borderSide: BorderSide.none,
+        ),
+        // 内容内边距
+        contentPadding: EdgeInsets.only(
+          top: 0,
+          bottom: 0,
         ),
       ),
+      // 校验
+      validator: validator,
     );
   }
 }
